a1d5cecda6f7c916676d10c68d1d3844f9b892e6
Bechara <b.elrammouz@equadis.com>
2024-06-12 21:59:53 +0300

    fix slider image

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home.service';
 import { Slider } from '../../shared/models/slider.interface';
 import { RouterLink } from '@angular/router';
-import { NgStyle } from '@angular/common';
+import { NgOptimizedImage, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
   standalone: true,
-  imports: [RouterLink, NgStyle],
+  imports: [RouterLink, NgStyle, NgOptimizedImage],
 })
 export class SliderComponent implements OnInit {
   public sliderItems: Slider[] = [];
