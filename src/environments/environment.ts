5d5083e39c1fe65fc4801d912f8e308f833045f2
Bechara <b.elrammouz@equadis.com>
2024-08-26 21:24:00 +0300

    update images url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000/api',
-  imagesUrl: 'http://localhost:4000/images',
+  imagesUrl: 'https://kitchinette-object-storage.fra1.digitaloceanspaces.com',
 };
