9c046ec551c76f7bfa338bba7c479a7d0b0c60dc
Bechara <b.elrammouz@equadis.com>
2024-08-26 23:01:43 +0300

    update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'https://kitchinette-back-icle7.ondigitalocean.app/api',
   imagesUrl: 'https://kitchinette-object-storage.fra1.digitaloceanspaces.com',
 };
