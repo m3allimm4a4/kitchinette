9ad3cda753200e9e053f74251336b281b46e8f84
Bechara <b.elrammouz@equadis.com>
2024-10-20 11:39:28 +0300

    update back.sh

--- scripts/back.sh
+++ scripts/back.sh
@@ -1,2 +1,2 @@
-docker image build -t m3allimm4a4/kitchinette-back:1 .
-docker image push m3allimm4a4/kitchinette-back:1
+docker image build -t m3allimm4a4/kitchinette-back:2 .
+docker image push m3allimm4a4/kitchinette-back:2
