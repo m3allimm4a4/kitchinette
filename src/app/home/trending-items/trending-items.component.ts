9e1219745c7f1cbda0d7bb10c5c16e683d1ea2d2
Bechara <b.elrammouz@equadis.com>
2024-11-03 12:22:09 +0200

    add carousel to trending products

--- src/app/home/trending-items/trending-items.component.ts
+++ src/app/home/trending-items/trending-items.component.ts
@@ -7,21 +7,40 @@ import { ProductCardComponent } from '../../shared/components/product-card/produ
 import { RouterLink } from '@angular/router';
 import { InitializationService } from '../../shared/services/initialization/initialization.service';
 import { forkJoin } from 'rxjs';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-trending-items',
   templateUrl: './trending-items.component.html',
   styleUrls: ['./trending-items.component.scss'],
   standalone: true,
-  imports: [NgClass, ProductCardComponent, RouterLink],
+  imports: [NgClass, ProductCardComponent, RouterLink, CarouselModule],
+  host: { ngSkipHydration: 'true' },
 })
 export class TrendingItemsComponent implements OnInit {
   public filteredTrendingItems: Product[] = [];
   public categories: Category[] = [];
   public activeCategory: Category = {} as Category;
   public showTab = false;
+  public responsiveOptions = [
+    {
+      breakpoint: '1400px',
+      numVisible: 4,
+      numScroll: 4,
+    },
+    {
+      breakpoint: '1220px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '1100px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+  ];
 
-  private trendingItems: Product[] = [];
+  public trendingItems: Product[] = [];
 
   constructor(
     private homeService: HomeService,
