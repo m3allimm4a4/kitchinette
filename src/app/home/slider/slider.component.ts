03357223571717ba991bcd2f16b224ced6b19922
Bechara <b.elrammouz@equadis.com>
2024-11-13 21:36:53 +0200

    fix bugs

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home.service';
 import { Slider } from '../../shared/models/slider.interface';
 import { RouterLink } from '@angular/router';
-import { NgOptimizedImage, NgStyle } from '@angular/common';
+import { NgStyle } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
   standalone: true,
-  imports: [RouterLink, NgStyle, NgOptimizedImage],
+  imports: [RouterLink, NgStyle],
 })
 export class SliderComponent implements OnInit {
+  protected readonly imagesUrl = environment.imagesUrl + '/';
   public sliderItems: Slider[] = [];
 
   constructor(private homeService: HomeService) {}
