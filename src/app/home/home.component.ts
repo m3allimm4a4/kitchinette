d9d39888c543ac7d1303b4f4c6b5573e6503b955
Bechara <b.elrammouz@equadis.com>
2024-11-16 13:18:30 +0200

    add policies section to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { SliderComponent } from './slider/slider.component';
 import { BannersComponent } from './banners/banners.component';
 import { TrendingItemsComponent } from './trending-items/trending-items.component';
+import { PoliciesComponent } from './policies/policies.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [SliderComponent, BannersComponent, TrendingItemsComponent],
+  imports: [SliderComponent, BannersComponent, TrendingItemsComponent, PoliciesComponent],
 })
 export class HomeComponent {}
